da99e3bb01d3b788a1fea78cd6c34276a2b715a8
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-08 23:03:42 +0100

    Update UserBean.swift

--- Diet Pro/Model/UserBean.swift
+++ Diet Pro/Model/UserBean.swift
@@ -13,39 +13,39 @@ class UserBean {
       errorList = [String]()
   }
 
-  func isCreateUserError(userName: String, gender: String, heights: Double, weights: Double, activityLevel: String, age: Double, targetCalories: Double, totalConsumedCalories: Double, bmr: Double) -> Bool {
-      resetData()
-      if userName == "" {
-          errorList.append("userName cannot be empty")
-      }
-      if gender == "" {
-          errorList.append("gender cannot be empty")
-      }
-      if heights != 0 {
-              errorList.append("heights cannot be zero")
-          }
-      if weights != 0 {
-              errorList.append("weights cannot be zero")
-          }
-      if activityLevel == "" {
-          errorList.append("activityLevel cannot be empty")
-      }
-      if age != 0 {
-              errorList.append("age cannot be zero")
-          }
-      if targetCalories != 0 {
-              errorList.append("targetCalories cannot be zero")
-          }
-      if totalConsumedCalories != 0 {
-              errorList.append("totalConsumedCalories cannot be zero")
-          }
-      if bmr != 0 {
-              errorList.append("bmr cannot be zero")
-          }
+    func isCreateUserError(user: User) -> Bool { 
+  	resetData() 
+  	if user.userName == "" {
+  		errorList.append("userName cannot be empty")
+  	}
+  	if user.gender == "" {
+  		errorList.append("gender cannot be empty")
+  	}
+  	if user.heights != 0 {
+	  		errorList.append("heights cannot be zero")
+	  	}
+  	if user.weights != 0 {
+	  		errorList.append("weights cannot be zero")
+	  	}
+  	if user.activityLevel == "" {
+  		errorList.append("activityLevel cannot be empty")
+  	}
+  	if user.age != 0 {
+	  		errorList.append("age cannot be zero")
+	  	}
+  	if user.targetCalories != 0 {
+	  		errorList.append("targetCalories cannot be zero")
+	  	}
+  	if user.totalConsumedCalories != 0 {
+	  		errorList.append("totalConsumedCalories cannot be zero")
+	  	}
+  	if user.bmr != 0 {
+	  		errorList.append("bmr cannot be zero")
+	  	}
 
     return errorList.count > 0
   }
-
+    
   func isEditUserError() -> Bool
     { return false }
           
